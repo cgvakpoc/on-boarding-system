9081176f423a39f6472395b2ed1953f2df7104ae
prakash.palanisamy <prakash.palanisamy@gbt-contractors.com>
2019-11-08 22:13:39 +0530

    Created the mail class and added the mail scheduling functionality

---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'IMAGE_UPLOAD_PATH' => 'uploads',
+    'ERROR_NOTIFICATION_FROM_EMAIL' => 'testmail@gmail.com',
+    'ERROR_NOTIFICATION_TO_EMAIL' => 'testmail5210@gmail.com'
+];
